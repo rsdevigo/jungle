79ccb754f58316a30e98c803b85e00dfbe62d7ef
Rodrigo Sanches Devigo <rsdevigo@gmail.com>
2015-06-18 14:57:39 -0400

    some fix in dashboard and add the start of How To connect

--- modules/core/controllers/home.client.controller.js
+++ modules/core/controllers/home.client.controller.js
@@ -4,6 +4,7 @@
 angular.module('core').controller('HomeController', ['$scope', '$http', '$localStorage', 'PLUGINSAVAILABLE',
 	function($scope, $http, $localStorage, PLUGINSAVAILABLE) {
 		$scope.storage = $localStorage;
+		$scope.isArray = angular.isArray;
 		$scope.pluginsAvailable = PLUGINSAVAILABLE;
 		if ($localStorage.kongurl !== undefined) {
 			$http.get($localStorage.kongurl).
